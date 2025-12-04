50b9d00c77a51ab6528d29bf23396ad11cc0c362
ToufiqLizon <lijonlijon30@gmail.com>
2025-12-04 11:14:14 +0600

    update

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../assets/banner.png'
+import Banner from '../assets/kuakataTour.png'
 import Image from '../compnent/Image'
 const Hero = () => {
   return (
