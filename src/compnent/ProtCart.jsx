75bbaf78b5a5c84e3d4482c66bcd8519233d4ed4
ToufiqLizon <lijonlijon30@gmail.com>
2025-12-21 00:33:07 +0600

    respons

--- src/compnent/ProtCart.jsx
+++ src/compnent/ProtCart.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ProtCart = ({hed,pra}) => {
   return (
-    <div className='rounded-[20px] border border-[#3C3C3C] bg-black shadow-[0_0_20px_rgba(78,255,119,0.15)] py-2 px-6'>
+    <div className='rounded-[20px] border border-[#3C3C3C] bg-black shadow-[0_0_20px_rgba(78,255,119,0.15)] py-2 px-6 '>
 <h3 className='text-center text-[36px] text-[#FFFFFF] font-bold font-exo '>{hed}</h3>
-<p className='text-center text-[20px] text-[#FFFFFF] font-normal font-exo w-[450px]'>{pra}</p>
+<p className='text-center text-[20px] text-[#FFFFFF] font-normal font-exo md:w-[450px] w-[350px]'>{pra}</p>
   </div>
   )
 }
